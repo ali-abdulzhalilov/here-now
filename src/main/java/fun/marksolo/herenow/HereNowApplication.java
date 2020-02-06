2d2b40c9206af91368ef840c0cf6b939c2f9d62d
mark-solo <mark.an.impostor@gmail.com>
2020-02-06 12:41:06 +0300

    Init spring application core

---
+++ src/main/java/fun/marksolo/herenow/HereNowApplication.java
@@ -0,0 +1,13 @@
+package fun.marksolo.herenow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HereNowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HereNowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fun/marksolo/herenow/HereNowApplicationTests.java
@@ -0,0 +1,13 @@
+package fun.marksolo.herenow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HereNowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
